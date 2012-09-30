7e954b9821e7928a2862974be3309edc1fc8629a
Maximo <vezzmax@gmail.com>
2012-09-29 23:26:10 -0300

    Initial project import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Vencimientos::Application.config.secret_token = 'cf8d19b3c6193ae88cee84566f0fcd8722a1f5f5e8a63b2e26b2d43b456357dfa5243b19f2bd1aeb3d797aa5578573d6dea2cbe831615a270b45c9e7f96a0fdd'
