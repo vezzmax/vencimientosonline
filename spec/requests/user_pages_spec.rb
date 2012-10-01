2d863386d0aa40507130008cf65d03ab4c238670
Maximo Vezzosi <mvezzosi@starmount.com>
2012-10-01 11:20:23 -0300

    Changed the h1 test text

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -6,7 +6,7 @@ describe "UserPages" do
   describe "pagina de registracion" do
     before { visit registrarse_path }
 
-    it { should have_selector('h1',    text: 'Registrarse') }
+    it { should have_selector('h1',    text: 'Registrar Usuario') }
     #it { should have_selector('title', text: full_title('Sign up')) }
   end
 end
