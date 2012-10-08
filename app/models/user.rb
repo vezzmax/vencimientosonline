310504cf2256b5d1ec8ae4e2cbdc10a816f71084
Maximo <vezzmax@gmail.com>
2012-10-08 17:17:09 -0300

    Email uniqueness in class and db. Adding index to find by email

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,10 +12,12 @@
 #
 
 class User < ActiveRecord::Base
-  attr_accessible :email, :name
-  validates :name, presence: true, length: { maximum: 50 }
-  
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX}
   
+  attr_accessible :email, :name
+
+  validates :name, presence: true, length: { maximum: 50 }
+  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX}, uniqueness: { case_sensitive: false }
+  
+  before_save { |user| user.email = email.downcase }
 end
