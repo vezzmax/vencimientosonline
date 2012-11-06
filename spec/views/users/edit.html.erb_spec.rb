4226723366971b8d31bb4a59f0bea9fc8800f323
Maximo <vezzmax@gmail.com>
2012-11-06 00:58:53 -0300

    regenerated user crud views

--- spec/views/users/edit.html.erb_spec.rb
+++ spec/views/users/edit.html.erb_spec.rb
@@ -2,7 +2,10 @@ require 'spec_helper'
 
 describe "users/edit" do
   before(:each) do
-    @user = assign(:user, stub_model(User))
+    @user = assign(:user, stub_model(User,
+      :name => "MyString",
+      :email => "MyString"
+    ))
   end
 
   it "renders the edit user form" do
@@ -10,6 +13,8 @@ describe "users/edit" do
 
     # Run the generator again with the --webrat flag if you want to use webrat matchers
     assert_select "form", :action => users_path(@user), :method => "post" do
+      assert_select "input#user_name", :name => "user[name]"
+      assert_select "input#user_email", :name => "user[email]"
     end
   end
 end
